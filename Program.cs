c01040fab9c8b6234c616360116a763fcb143720
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-07-11 17:57:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nested_loops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N: ");
+            double n = double.Parse(Console.ReadLine());
+           
+
+            for(int i = 1111; i <= 9999; i++)
+            {
+                int br = 0;
+                for (int j = i; j > 0; j = j / 10)
+                {
+                   
+                    if (n % (j % 10)==0)
+                    {
+                        br++;
+                    }
+                }
+
+                if (br == 4)
+                {
+                    Console.Write("{0} ", i);
+                }
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
